f1b06fe8c98ff64b9d490a33db7ab83cbec85e01
yuvraj sharma <sharma.yuvraj.00786@gmail.com>
2024-05-12 14:48:07 +0100

    updated requirements.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import mysql.connector
+import pymysql
 
 Base = declarative_base()
 
